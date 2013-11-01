3b8e9264ed627546727ce988f983c342584faf72
Nic0w <nico.broquet@gmail.com>
2013-11-01 23:41:50 +0100

    not working :(

---
+++ src/main/fr/esiea/ihm/scm/controllers/TestController.java
@@ -0,0 +1,23 @@
+package fr.esiea.ihm.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void helloWorld() {
+
+		System.out.println("OK !!");
+	}
+
+}
