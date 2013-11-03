d169354b270a98f945a43c150a800f0f3b8008db
Nic0w <broquet@et.esiea.fr>
2013-11-03 19:29:14 +0100

    Forgot about add & remove in ContactDAO & ContactService

--- src/main/java/fr/esiea/ail/ihm/scm/controller/ContactController.java
+++ src/main/java/fr/esiea/ail/ihm/scm/controller/ContactController.java
@@ -40,7 +40,8 @@ public class ContactController implements CrudController<Contact> {
     @ResponseStatus(HttpStatus.CREATED)
 	@Override
 	public void create(@RequestBody Contact contact) throws DAOException {
-		
+    	LOGGER.info("Add !");
+    	
 		this.contactService.add(contact);
 	}
 
@@ -58,8 +59,9 @@ public class ContactController implements CrudController<Contact> {
     @ResponseStatus(HttpStatus.OK)
 	@Override
 	public void remove(@PathVariable("id") Integer id) throws DAOException {
+		LOGGER.info("Remove !");
 		
-		
+		this.contactService.remove(id);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
