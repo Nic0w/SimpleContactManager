533e27e4164da037bfe324be7ce1c73ba2b52ed8
Thomas BEJUIT <thomas@gmail.com>
2013-11-03 22:18:53 +0100

    started to synchronize the contact list

--- webapp/js/Ctrls.js
+++ webapp/js/Ctrls.js
@@ -1,12 +1,13 @@
 var smcApp_Controllers = angular.module('SCMApp_Controllers', ['ui.bootstrap']);
-
+//controller for the sliding panel connection
 smcApp_Controllers.controller('SlideCtrl',['$scope',function($scope) {$scope.isCollapsed = true;}]);
 
+/*Controller of the create contact Page*/
 smcApp_Controllers.controller('NewContactCtrl',['$scope','$http',function($scope,$http){
   $scope.contact={};
-  $scope.contact.addr=new Array;
+  $scope.contact.addr=new Array; // we create a new array in prevision to add multiple addresses
   $scope.contact.addr[0]={};
-  $scope.contact.emails=new Array;
+  $scope.contact.emails=new Array; // idem but for emails
   $scope.contact.phones={};
   $http.get('js/datas/countries.json').success(function(data) {
     $scope.countries = data;
@@ -17,3 +18,11 @@ smcApp_Controllers.controller('NewContactCtrl',['$scope','$http',function($scope
   }
 
 }]);
+//Controller for the contact list
+smcApp_Controllers.controller('ContactListCtrl',['$scope','$http',function($scope,$http){
+
+$http.get('/contact').success(function(data){
+$scope.contactList=data;
+});
+}])
+
