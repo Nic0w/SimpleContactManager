faa42243aac400f4493550c77497604b4cd0b345
Nic0w <broquet@et.esiea.fr>
2013-11-04 01:22:39 +0100

    Adding support of users (Sign In, Sign Up)

---
+++ src/main/java/fr/esiea/ail/ihm/scm/service/UserService.java
@@ -0,0 +1,40 @@
+package fr.esiea.ail.ihm.scm.service;
+
+import java.util.Collection;
+
+import fr.esiea.ail.ihm.scm.controller.handler.ApplicationException;
+import fr.esiea.ail.ihm.scm.model.user.User;
+
+public class UserService implements IService<User> {
+
+	@Override
+	public User getOne(Integer id) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(User model) throws ApplicationException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(User model) throws ApplicationException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Integer id) throws ApplicationException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<User> getAll() throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/fr/esiea/ail/ihm/scm/service/ident/IdentificationService.java
@@ -0,0 +1,5 @@
+package fr.esiea.ail.ihm.scm.service.ident;
+
+public interface IdentificationService {
+
+}
